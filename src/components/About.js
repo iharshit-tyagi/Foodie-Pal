d34b3dc44cae6ebd7f638df4cf8c497be7d02a56
Harshit Tyagi <harshitt152@gmail.com>
2023-10-28 19:51:30 +0530

    Update About Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,55 @@
 const About = () => {
   return (
-    <div>
-      <h1>About us Page</h1>
-      <p>fnrgo;erng;oteng</p>
+    <div className="grid grid-rows-3 grid-cols-1 gap-4 w-2/3 mx-auto text-center  mt-5">
+      <div className=" p-4 bg-orange-50">
+        <h1 className="text-3xl font-bold">About Us</h1>
+        <p className="mt-5 text-lg">
+          Welcome to <span className="font-bold ">Foodie Pal</span> , your go-to
+          destination for delicious food delivered right to your doorstep! We
+          are passionate about food and dedicated to providing you with a
+          delightful dining experience.
+        </p>
+      </div>
+      <div className="bg-gray-200 p-4">
+        <h1 className="text-3xl font-bold">Our Story</h1>
+        <p className="mt-5 text-lg">
+          Foodie Pal was founded with a simple yet powerful mission: to connect
+          food lovers with exceptional restaurants, bringing you a diverse
+          selection of cuisines from around the world. Our journey began with
+          the belief that great food should be easily accessible to everyone, no
+          matter where you are.
+        </p>
+      </div>
+      <div className="bg-slate-100 p-4 mb-4">
+        <h1 className="text-3xl font-bold">Get in Touch</h1>
+        <p className="mt-5 text-lg">
+          Have questions, suggestions, or feedback? We'd love to hear from you!
+          Feel free to contact our customer support team through our app, or
+          reach out to us via email at foodiePal@xyz.com. Thank you for choosing
+          Foodie Pal as your food delivery partner. We look forward to serving
+          you and satisfying your culinary cravings. Bon appétit!
+        </p>
+      </div>
     </div>
+
+    // <div className="flex flex-col">
+    //   <h1 className="font-bold text-2xl mt-6 text-center">About Us</h1>
+    //   <div className="bg-gray-300 mt-6 w-3/5">
+    //     <p className="px-4 text-xl">
+    //       Welcome to <span className="font-bold ">Foodie Pal</span> , your go-to
+    //       destination for delicious food delivered right to your doorstep! We
+    //       are passionate about food and dedicated to providing you with a
+    //       delightful dining experience.
+    //     </p>
+    //     <p className="mt-6 px-4 text-xl">
+    //       <span className="font-bold ">Foodie Pal</span> was founded with a
+    //       simple yet powerful mission: to connect food lovers with exceptional
+    //       restaurants, bringing you a diverse selection of cuisines from around
+    //       the world. Our journey began with the belief that great food should be
+    //       easily accessible to everyone, no matter where you are.
+    //     </p>
+    //   </div>
+    // </div>
   );
 };
 export default About;
--- src/utils/useRestaurantList.js
+++ src/utils/useRestaurantList.js
@@ -12,13 +12,16 @@ const useRestaurantList = () => {
   );
 
   useEffect(() => {
-    fetchData();
+    fetchData().catch((err) => {
+      console.log(err);
+    });
   }, []);
 
   const fetchData = async () => {
     // const data = await fetch(SWIGGY_API_URL_AGRA);
     // const data = await fetch(SWIGGY_API_URL_Delhi);
     const data = await fetch(SWIGGY_API_URL_PUNE);
+
     const jsonData = await data.json();
     setListOfRestaurants(
       jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
